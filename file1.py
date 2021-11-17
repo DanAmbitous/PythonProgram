e3eb9fdc369e2ca3893206773386b22096293c5a
Danial Azadpour <danazad2016@gmail.com>
2021-11-16 23:05:52 -0600

    Initial Commit

---
+++ file1.py
@@ -0,0 +1,48 @@
+import math
+
+def unitOfWeight(run):
+  if (run):
+    userInput = input("Would you want to kg or lbs?: ")
+
+    if (userInput == "kg"):
+      weightTeller(True)
+    elif (userInput == "lbs"):
+      weightTeller(False)
+    else:
+      print(f"Invalid input of {userInput}")
+      unitOfWeight()
+  else:
+    print("Thank you for using our program")
+
+def weightTeller(kg):
+  if (kg):
+    kgInput = float(input("Input the weight in kgs: "))
+    print(not isinstance(kgInput, (int, float)))
+    if (not isinstance(kgInput, (int, float))):
+      print('Does this run?')
+      weightTeller(kg)
+      return
+
+    kgInput = int(kgInput)
+
+    print(kgInput)
+    lbs = round(kgInput * 2.2)
+    print(f"The weight of {kgInput}kg is {lbs}lbs")
+    toRedo()
+  else:
+    lbsInput = input("Input the weight in lbs: ")
+
+    if (isinstance(lbsInput, (int, float))):
+      weightTeller(kg)
+
+    lbsInput = int(lbsInput)
+
+    kg = round(lbsInput / 2.2)
+    print(f"The weight of {lbsInput}lbs is {kg}lbs")
+    toRedo()
+
+def toRedo():
+  userInput = bool(input("Would you like to redo? (True, False): "))
+  unitOfWeight(userInput)
+
+unitOfWeight(True)

0678fbe36a1e8e1c559a891ebdfeb80c007f5bbe
Danial Azadpour <danazad2016@gmail.com>
2021-11-24 17:52:49 -0600

    feat
    Added the car game

--- weightConversion.py
+++ weightConversion.py
@@ -1,65 +1,48 @@
-class UnitType:
-  def __init__(self, name, inputName):
-      self.name = name
-      self.inputName = inputName
-
-KG = UnitType('kilograms', '1')
-LBS = UnitType('pounds', '0')
-
-def strIntConversion(strInput):
-  try:
-    return int(strInput)
-  except:
-    pass
+cmds = 'start stop quit help'
+cmds = cmds.split(' ')
 
 
-def weightUnitPickining(initialRun):
-  if (initialRun):
-    print(f"Welcome to {KG.name} and {LBS.name} converter program!")
 
-  weightUnit = input(f"Would you like to convert from {KG.name}(1) or {LBS.name}(0)?: ")
+def commandRunning():
+  carStatus = 'stop'
 
-  if (weightUnit == KG.inputName or LBS.inputName):
-    weightConverter(weightUnit)
-  else: 
-    print('Your input is invalid')
+  while True:
+    userCmd = input('> ')
+    
+    match userCmd:
+      case 'start':
+        if (carStatus == 'stop'):
+          print('Starting the car')
+          carStatus = 'start'
+        else:
+          print('Car is already running')
+      case 'stop':
+        if (carStatus == 'start'):
+          print('Stopping the car')
+          carStatus = 'stop'
+        else:
+          print('Car is already stopped')
+      case 'quit':
+        print('Thanks for playing')
+        playAgain(False)
+        return
+      case 'help':
+        print('The commands are start, stop, help, and quit')
+      case _:
+        print('Did not understand the command')
 
-def weightConverter(weight):
-  if (weight == "1"):
-    print(f"You've chosen to convert from {KG.name} to {LBS.name}")
-  
-    while True:
-      try:
-        fromKg = int(input(f"Input the {KG.name} you'd like to convert from: "))
-        print(f"{fromKg} {KG.name} is {round(fromKg*2.2)} {LBS.name}")
-        redo()
-        break
-      except:
-        print('Invalid input')
-        pass
-  else:
-    print(f"You've chosen to convert from {LBS.name} to {KG.name}")
-  
-    while True:
-      try:
-        fromLBS = int(input(f"Input the {LBS.name} you'd like to convert from: "))
-        print(f"{fromLBS} {LBS.name} is {round(fromLBS/2.2)} {KG.name}")
-        redo()
-        break
-      except:
-        print('Invalid input')
-        pass
+def playAgain(remessage):
+  if (remessage):
+    print('Did not understand')
 
-def redo():
-  restart = input('Would you like to restart the program? yes(1) no(0): ')
+  toReplay = input('Would you like to replay? (y/n): ')
+  print(toReplay)
+  match toReplay:
+    case 'y':
+      commandRunning()
+    case 'n':
+      return
+    case _:
+      playAgain(True)
 
-  if (restart == '1'):
-    weightUnitPickining(False)
-  elif (restart == '0'):
-    print('Thanks for using the it, and have a nice day!')
-    return
-  else:
-    print('Invalid input')
-    redo()
-
-weightUnitPickining(True)
+commandRunning()

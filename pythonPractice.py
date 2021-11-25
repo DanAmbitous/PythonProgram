bb52bdcb0062af17f772fae8b8b76e6a58010d94
Danial Azadpour <danazad2016@gmail.com>
2021-11-25 13:29:45 -0600

    Unique numbers from arrays

--- pythonPractice.py
+++ pythonPractice.py
@@ -1,25 +1,8 @@
-from random import randint
+numbers = [5, 10, 5, 12, 1, 23]
+uniqueNumbers = []
 
-numValues = []
+for number in numbers:
+  if (number not in uniqueNumbers):
+    uniqueNumbers.append(number)
 
-while len(numValues) < 5:
-  numValues.append(randint(0, 10))
-
-# numValues.sort()
-
-# print(numValues, numValues[-1])
-
-# biggestNumber = max(numValues)
-
-# print(numValues, biggestNumber)
-
-def maxFinder(list):
-  max = list[0]
-
-  for i in list:
-    if (i > max):
-      max = i
-  
-  return max
-
-print(maxFinder(numValues))
+print(uniqueNumbers)

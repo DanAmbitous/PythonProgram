82f3d2c3bd7bb46875fd0d050a72d738c2a46cf7
Danial Azadpour <danazad2016@gmail.com>
2021-11-24 19:14:58 -0600

    feat
    f counter 2

--- pythonPractice.py
+++ pythonPractice.py
@@ -2,27 +2,37 @@ code = [5, 2, 5, 2, 2]
 shape = 'خ'
 o = 0
 
+# Method 1
+for x in code:
+  output = ''
+  for i in range(x):
+    output += 'a'
+  print(output)
+
+
+# Method 2
 for i in code:
   print('x' * i)
 
-# for i in range(code[o] + 1):
-#   if (i == code[o]):
-#     print(shape * i)
-#     o += 1
-#     for ii in range(code[o] + 1):
-#       if (ii == code[o]):
-#         print(shape * ii) 
-#         o += 1
-#         for iii in range(code[o] + 1):
-#           if (iii == code[o]):
-#             print(shape * iii) 
-#             o += 1
-#             for iv in range(code[o] + 1):
-#               if (iv == code[o]):
-#                 print(shape * iv)   
-#                 o += 1
-#                 for v in range(code[o] + 1):
-#                   if (v == code[o]):
-#                     print(shape * v) 
-#                     o += 1
+# Method 1
+for i in range(code[o] + 1):
+  if (i == code[o]):
+    print(shape * i)
+    o += 1
+    for ii in range(code[o] + 1):
+      if (ii == code[o]):
+        print(shape * ii) 
+        o += 1
+        for iii in range(code[o] + 1):
+          if (iii == code[o]):
+            print(shape * iii) 
+            o += 1
+            for iv in range(code[o] + 1):
+              if (iv == code[o]):
+                print(shape * iv)   
+                o += 1
+                for v in range(code[o] + 1):
+                  if (v == code[o]):
+                    print(shape * v) 
+                    o += 1
           
